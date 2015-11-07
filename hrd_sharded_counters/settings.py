3ab5f57ea8e7b238b1f8fb9b4870ccb727a8930e
Ashish Kedia <ashish1294@gmail.com>
2015-11-07 14:54:49 +0530

    Template Settings Added

--- hrd_sharded_counters/settings.py
+++ hrd_sharded_counters/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'hrd_sharded_counters.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.join(BASE_DIR, 'shard_app'), 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,3 +100,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
