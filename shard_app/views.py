7079c877b36871219aa090eeb7036c6aa537293d
Ashish Kedia <ashish1294@gmail.com>
2016-02-05 03:28:52 +0530

    Fixed Unsharded Idempotency Bug

--- shard_app/views.py
+++ shard_app/views.py
@@ -21,11 +21,14 @@ def increment_unsharded_counter(delta, request_id):
   if log_key.get() is not None:
     return
 
-  IncrementTransaction.get_or_insert(request_id)
-
   counter = IOC.IncrementOnlyShard.get_or_insert(UNSHARDED_COUNTER_KEY)
   counter.count += delta
   counter.put()
+
+  # Adding Transaction Log Here
+  trx = IncrementTransaction.get_or_insert(request_id)
+  trx.shard_key = counter.key
+  trx.put()
   memcache.incr(UNSHARDED_COUNTER_KEY, delta=delta)
 
 def unsharded_counter_value():
